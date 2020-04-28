e9a2b1acd0eb6d69ebefb929264a3cdcff5c3fc4
wyf <wyf666>
2020-04-28 11:18:35 +0800

    常量中删除无效变量

--- src/main/java/com/bcht/data_manager/consts/Constants.java
+++ src/main/java/com/bcht/data_manager/consts/Constants.java
@@ -14,51 +14,6 @@ public final class Constants {
      */
     public static final String MSG = "msg";
 
-    /**
-     * data total
-     * 数据总数
-     */
-    public  static final String COUNT = "count";
-
-    /**
-     * page size
-     * 每页数据条数
-     */
-    public  static final String PAGE_SIZE = "pageSize";
-
-    /**
-     * current page no
-     * 当前页码
-     */
-    public  static final String PAGE_NUMBER = "pageNo";
-
-    /**
-     * result
-     */
-    public static final String RESULT = "result";
-
-    /**
-     *
-     */
-    public static final String DATA_LIST = "data";
-
-    public static final String TOTAL_LIST = "totalList";
-
-    public static final String CURRENT_PAGE = "currentPage";
-
-    public static final String TOTAL_PAGE = "totalPage";
-
-    public static final String TOTAL = "total";
-
-    public static final String SPRING_DATASOURCE_FILE = "data_source.properties";
-
-    public static final String HADOOP_PROPERTIES_PATH = "";
-
-    public static final String ESCHEDULER_ENV_PATH = "";
-
-    public static final String SCHEDULER_QUEUE_IMPL = "";
-
-    public static final String DEVELOPMENT_STATE = "";
 
     /** data source **/
 
@@ -68,41 +23,9 @@ public final class Constants {
 
     public static final String SPRING_DATASOURCE_PASSWORD = "spring.datasource.password";
 
-    public static final String SPRING_DATASOURCE_TYPE = "spring.datasource.type";
-
     public static final String SPRING_DATASOURCE_DRIVER_CLASS_NAME = "spring.datasource.driver-class-name";
 
     public static final String SPRING_DATASOURCE_VALIDATION_QUERY = "";
-
-    public static final String SPRING_DATASOURCE_POOL_PREPARED_STATEMENTS = "";
-
-    public static final String SPRING_DATASOURCE_TEST_WHILE_IDLE = "";
-
-    public static final String SPRING_DATASOURCE_TEST_ON_BORROW = "";
-
-    public static final String SPRING_DATASOURCE_TEST_ON_RETURN = "";
-
-
-
-    public static final String SPRING_DATASOURCE_MIN_IDLE = "";
-
-    public static final String SPRING_DATASOURCE_MAX_ACTIVE = "";
-
-    public static final String SPRING_DATASOURCE_MAX_WAIT = "";
-
-    public static final String SPRING_DATASOURCE_MAX_POOL_PREPARED_STATEMENT_PER_CONNECTION_SIZE = "";
-
-    public static final String SPRING_DATASOURCE_INITIAL_SIZE = "";
-
-    public static final String SPRING_DATASOURCE_TIME_BETWEEN_EVICTION_RUNS_MILLIS = "";
-
-    public static final String SPRING_DATASOURCE_TIME_BETWEEN_CONNECT_ERROR_MILLIS = "";
-
-    public static final String SPRING_DATASOURCE_MIN_EVICTABLE_IDLE_TIME_MILLIS = "";
-
-    public static final String SPRING_DATASOURCE_VALIDATION_QUERY_TIMEOUT = "";
-
-    public static final String SPRING_DATASOURCE_DEFAULT_AUTO_COMMIT = "";
 
     public static final String DEVELOPMENT = "";
 
@@ -113,19 +36,12 @@ public final class Constants {
 
     public static final String SESSION_ID = "sessionId";
 
-    public static final String PASSWORD_DEFAULT = "******";
-
 
     /**
      * session timeout
      */
     public static final int SESSION_TIME_OUT = 7200;
     public static final int maxFileSize = 1024 * 1024 * 1024;
-    public static final String UDF = "UDF";
-    public static final String CLASS = "class";
-    public static final String RECEIVERS = "receivers";
-    public static final String RECEIVERS_CC = "receiversCc";
-
 
     /**
      * driver
@@ -138,4 +54,10 @@ public final class Constants {
     public static final Integer CREATE_TABLE_METHOD_OF_CREATE_SQL = 1;
 
     public static final Integer CREATE_TABLE_METHOD_OF_COLUMN_COMPOSE = 2;
+
+
+    /**
+     * external api data interface
+     */
+    public static final String CLOUDEAR_HDFS_NAMENODE_URL = "cloudera.hdfs.namenode.url";
 }
